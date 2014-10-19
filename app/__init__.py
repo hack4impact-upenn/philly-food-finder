b7afdb4734398ff441af2666bddc5c2c703decb0
Ben Sandler <ben1sandler@gmail.com>
2014-10-19 14:09:42 -0400

    deleted ali's code

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
-
-#commented out because might be used eventually
-#from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
 from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-#lm = LoginManager()
-lm.init_app(app)
-lm.login_view = "login"
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
