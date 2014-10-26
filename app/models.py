43d0443a2ff63a80e338e2b60e229c6e919d7ac0
Ben Sandler <ben1sandler@gmail.com>
2014-10-26 18:00:30 -0400

    Updated TimeSlot, Address, and Resource models. Created relationships between them. Added two tests for Resources

--- app/models.py
+++ app/models.py
@@ -1,30 +1,29 @@
 from app import db
 
 class Address(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    line1 = db.Column(db.String(100))
-    line2 = db.Column(db.String(100))
-    city = db.Column(db.String(35))
-    state = db.Column(db.String(2))
-    zip_code = db.Column(db.String(5))
+	id = db.Column(db.Integer, primary_key = True)
+	line1 = db.Column(db.String(100))
+	line2 = db.Column(db.String(100))
+	city = db.Column(db.String(35))
+	state = db.Column(db.String(2))
+	zip_code = db.Column(db.String(5))
+	resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'))
+
+class TimeSlot(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	day_of_week = db.Column(db.Integer)
+	start_time = db.Column(db.Time)
+	end_time = db.Column(db.Time)
+	resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'))
 
 class Resource(db.Model):
 	location_type_enums = ('FARMER_MARKET','MEALS_ON_WHEELS','FOOD_CUPBOARDS_LIST','SHARE','SOUP_KITCHEN','WIC_OFFICES')
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50))
-    address = db.Column(Address)
-    phone = db.Column(db.String(35))
-    timeslot_0 = db.Column(TimeSlot)
-    timeslot_1 = db.Column(TimeSlot)
-    timeslot_2 = db.Column(TimeSlot)
-    timeslot_3 = db.Column(TimeSlot)
-    timeslot_4 = db.Column(TimeSlot)
-    timeslot_5 = db.Column(TimeSlot)
-    timeslot_6 = db.Column(TimeSlot)
-    description = db.Column(db.String(500))
-    location_type = db.Column(Enum(*location_type_enums))
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(50))
+	address = db.relationship('Address', backref='resource', lazy='select', uselist=False)
+	phone = db.Column(db.String(35))
+	timeslots = db.relationship('TimeSlot', backref='resource', lazy='select', uselist=True)
+	description = db.Column(db.String(500))
+	location_type = db.Column(db.Enum(*location_type_enums))
 
-class TimeSlot(db.Model):
-	day_of_week = dbColumn(db.Ingeger)
-	start_time = dbColumn(db.Time)
-	end_time = dbColumn(db.Time)
+
--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 #!flask/bin/python
 import unittest
 import os
+from datetime import time
+from sqlalchemy.exc import IntegrityError
 from app import app, db
 from config import basedir
-from app.models import Address
+from app.models import Address, Resource, TimeSlot
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -25,6 +27,34 @@ class TestCase(unittest.TestCase):
     	db.session.commit()
     	assert len(Address.query.filter_by(zip_code = '14109').all()) == 2
     	assert len(Address.query.filter_by(zip_code = '14109', city = 'New York').all()) == 0
+
+    def test_create_valid_resource(self):
+        a1 = Address(line1 = '1234 MB 1234', line2 = '3700 Spruce St', city = 'Philadelphia', state = 'PA', zip_code = '14109')
+        t0 = TimeSlot(day_of_week=0,start_time=time(8,0),end_time=time(18,30))
+        t1 = TimeSlot(day_of_week=1,start_time=time(7,0),end_time=time(19,30))
+        t2 = TimeSlot(day_of_week=2,start_time=time(7,30),end_time=time(18,30))
+        t3 = TimeSlot(day_of_week=3,start_time=time(8,0),end_time=time(19,30))
+        t4 = TimeSlot(day_of_week=4,start_time=time(10,0),end_time=time(5,30))
+        t5 = TimeSlot(day_of_week=5,start_time=time(8,15),end_time=time(18,45))
+        t6 = TimeSlot(day_of_week=6,start_time=time(9,0),end_time=time(20,45))
+        desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem neque, vehicula ac nisl at, porta porttitor enim. Suspendisse nibh eros, pulvinar nec risus a, dignissim efficitur diam. Phasellus vestibulum posuere ex, vel hendrerit turpis molestie sit amet. Nullam ornare magna quis urna sodales, vel iaculis purus consequat. Mauris laoreet enim ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. In et dui ante. Morbi elementum dolor ligula, et mollis magna interdum non. Mauris ligula mi, mattis at ex ut, pellentesque porttitor elit. Integer volutpat elementum tristique. Ut interdum, mauris a feugiat euismod, tortor.'
+        r1 = Resource(name='Test Resource 1',address=a1,phone='1234567898',timeslots=[t0,t1,t2,t3,t4,t5,t6],description=desc,location_type='FARMER_MARKET')
+        db.session.add(r1)
+        db.session.commit()
+        assert len(Resource.query.filter_by(name = 'Test Resource 1').all()) == 1
+        assert len(Resource.query.filter_by(address = a1).all()) == 1
+
+    def test_create_invalid_enum_resource(self):
+        a1 = Address(line1 = '1234 MB 1234', line2 = '3700 Spruce St', city = 'Philadelphia', state = 'PA', zip_code = '14109')
+        t0 = TimeSlot(day_of_week=0,start_time=time(8,0),end_time=time(18,30))
+        t1 = TimeSlot(day_of_week=1,start_time=time(7,0),end_time=time(19,30))
+        t2 = TimeSlot(day_of_week=2,start_time=time(7,30),end_time=time(18,30))
+        t3 = TimeSlot(day_of_week=3,start_time=time(8,0),end_time=time(19,30))
+        t4 = TimeSlot(day_of_week=4,start_time=time(10,0),end_time=time(5,30))
+        t5 = TimeSlot(day_of_week=5,start_time=time(8,15),end_time=time(18,45))
+        t6 = TimeSlot(day_of_week=6,start_time=time(9,0),end_time=time(20,45))
+        desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem neque, vehicula ac nisl at, porta porttitor enim. Suspendisse nibh eros, pulvinar nec risus a, dignissim efficitur diam. Phasellus vestibulum posuere ex, vel hendrerit turpis molestie sit amet. Nullam ornare magna quis urna sodales, vel iaculis purus consequat. Mauris laoreet enim ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. In et dui ante. Morbi elementum dolor ligula, et mollis magna interdum non. Mauris ligula mi, mattis at ex ut, pellentesque porttitor elit. Integer volutpat elementum tristique. Ut interdum, mauris a feugiat euismod, tortor.'
+        self.assertRaises(IntegrityError, r1 = Resource(name='Test Resource 1',address=a1,phone='1234567898',timeslots=[t0,t1,t2,t3,t4,t5,t6],description=desc,location_type='WRONG_ENUM!!!!!!!!!!!!!'))
 
 if __name__ == '__main__':
     unittest.main()
