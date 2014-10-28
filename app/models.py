dcda0ef5dda86d275d880a2504924424b3c0fc12
Ben Sandler <ben1sandler@gmail.com>
2014-10-28 00:13:21 -0400

    added documentation to tests, created central function to create globals, renamed resource to food resource

--- app/models.py
+++ app/models.py
@@ -7,23 +7,23 @@ class Address(db.Model):
 	city = db.Column(db.String(35))
 	state = db.Column(db.String(2))
 	zip_code = db.Column(db.String(5))
-	resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'))
+	resource_id = db.Column(db.Integer, db.ForeignKey('food_resource.id'))
 
 class TimeSlot(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	day_of_week = db.Column(db.Integer)
 	start_time = db.Column(db.Time)
 	end_time = db.Column(db.Time)
-	resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'))
+	resource_id = db.Column(db.Integer, db.ForeignKey('food_resource.id'))
 
-class Resource(db.Model):
-	location_type_enums = ('FARMER_MARKET','MEALS_ON_WHEELS','FOOD_CUPBOARDS_LIST','SHARE','SOUP_KITCHEN','WIC_OFFICES')
+class FoodResource(db.Model):
+	food_resource_type_enums = ('FARMERS_MARKET','MEALS_ON_WHEELS','FOOD_CUPBOARD','SHARE','SOUP_KITCHEN','WIC_OFFICE')
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(50))
-	address = db.relationship('Address', backref='resource', lazy='select', uselist=False)
+	address = db.relationship('Address', backref='food_resource', lazy='select', uselist=False)
 	phone = db.Column(db.String(35))
-	timeslots = db.relationship('TimeSlot', backref='resource', lazy='select', uselist=True)
+	timeslots = db.relationship('TimeSlot', backref='food_resource', lazy='select', uselist=True)
 	description = db.Column(db.String(500))
-	location_type = db.Column(db.Enum(*location_type_enums))
+	location_type = db.Column(db.Enum(*food_resource_type_enums))
 
 
