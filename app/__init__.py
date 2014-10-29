98847105e37f2dcee543be294d76850fc602fc24
Ben Sandler <ben1sandler@gmail.com>
2014-10-29 14:16:05 -0400

    created rudimentary user model and basic tests

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+from app.models import User
+from flask_user import SQLAlchemyAdapter, UserManager
 
+db_adapter = SQLAlchemyAdapter(db, User)        # Register the User model
+user_manager = UserManager(db_adapter, app)     # Initialize Flask-User
