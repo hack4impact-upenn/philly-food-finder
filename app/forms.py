42b3380d1fde7839b8538aadd0ca4738b65a1f27
veronicawharton <veronica.wharton@yahoo.com>
2014-11-02 02:53:49 -0500

    Add form inheritance for RequestNewFoodResourceForm. Removed old forms code.

--- app/forms.py
+++ app/forms.py
@@ -2,34 +2,8 @@ from flask.ext.wtf import Form
 from wtforms import TextField, TextAreaField, validators
 from wtforms.validators import InputRequired, Length, URL, Email
 
-class RequestNewFoodResourceForm(Form):
-    # Information about the person submitting the food resource for evaluation. 
-    first_name = TextField(
-        label = 'Your First Name', 
-        validators = [
-            InputRequired("Please provide your first name."),
-            Length(0, 35)
-        ])
-    last_name = TextField(
-        label = 'Your Last Name', 
-        validators = [
-            InputRequired("Please provide your last name."),
-            Length(0, 35)
-        ])
-    email_address = TextField(
-        label = 'Your Email Address', 
-        validators = [
-            InputRequired("Please provide an email address at which we can contact you."), 
-            Email("Invalid email address."),
-            Length(1, 35)
-        ])
-    phone_number = TextField(
-        label = 'Your Phone Number', 
-        validators = [
-            InputRequired("Please provide a phone number at which we can contact you.")
-        ])
-
-    # Information about the food resource itself. 
+# Information about a new food resource. 
+class AddNewFoodResourceForm(Form):
     website = TextField(
         label = 'Food Resource Website', 
         validators = [
@@ -76,32 +50,30 @@ class RequestNewFoodResourceForm(Form):
             Length(1, 5) # Same max length as in Address model.
         ])
 
-#class AddNewFoodResourceForm(Form):
-
-
-
-# class LoginForm(Form):
-#   openid = TextField('openid', validators = [Required()])
-#   remember_me = BooleanField('remember_me', default = False)
-
-# class EditForm(Form):
-#     nickname = TextField('nickname', validators = [Required()])
-#     about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
-
-#     def __init__(self, original_nickname, *args, **kwargs):
-#         Form.__init__(self, *args, **kwargs)
-#         self.original_nickname = original_nickname
-
-#     def validate(self):
-#         if not Form.validate(self):
-#             return False
-#         if self.nickname.data == self.original_nickname:
-#             return True
-#         user = User.query.filter_by(nickname = self.nickname.data).first()
-#         if user != None:
-#             self.nickname.errors.append('This nickname is already in use. Please choose another one.')
-#             return False
-#         return True
-
-# class PostForm(Form):
-#     post = TextField('post', validators = [Required()])
+# Information about the person submitting the food resource for evaluation. 
+# Subclassed from 'AddNewFoodResourceForm'
+class RequestNewFoodResourceForm(AddNewFoodResourceForm):
+    first_name = TextField(
+        label = 'Your First Name', 
+        validators = [
+            InputRequired("Please provide your first name."),
+            Length(0, 35)
+        ])
+    last_name = TextField(
+        label = 'Your Last Name', 
+        validators = [
+            InputRequired("Please provide your last name."),
+            Length(0, 35)
+        ])
+    email_address = TextField(
+        label = 'Your Email Address', 
+        validators = [
+            InputRequired("Please provide an email address at which we can contact you."), 
+            Email("Invalid email address."),
+            Length(1, 35)
+        ])
+    phone_number = TextField(
+        label = 'Your Phone Number', 
+        validators = [
+            InputRequired("Please provide a phone number at which we can contact you.")
+        ])
