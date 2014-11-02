31d7de1c61a9f739af08856ea4a4589113a990e0
veronicawharton <veronica.wharton@yahoo.com>
2014-11-02 12:20:13 -0500

    Added scss invocation.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.scss import Scss
 import os
 from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+Scss(app)
 
 from app import views, models, forms
 
