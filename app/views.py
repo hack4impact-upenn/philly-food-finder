1da99c29f29b2dbfb534017e944acac63c48b870
natashanarang <nnarang777@gmail.com>
2014-11-02 11:19:13 -0500

    static map

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from app import app, db
 from models import Address
+from flask import render_template
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+@app.route('/map')
+def map():
+    return render_template('staticmap.html')
 
