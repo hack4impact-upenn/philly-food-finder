ce1132c2c7032cea63299104f964faa3c5da14ab
Ben Sandler <ben1sandler@gmail.com>
2014-11-03 17:59:56 -0500

    added login page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from app import app, db
 from models import Address
+from flask import render_template
 
 @app.route('/')
 def index():
     return "Hello World!"
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')
