e7927b64780a2c2a4067b5ebf6bbbcc41e49c56c
Ben Sandler <ben1sandler@gmail.com>
2014-11-06 00:42:41 -0500

    have basic wtfforms working

--- app/views.py
+++ app/views.py
@@ -1,24 +1,22 @@
 from app import app, db
 from models import Address
-from forms import RequestNewFoodResourceForm
+from forms import RequestNewFoodResourceForm, LoginForm
 from flask import render_template, flash, redirect, session, url_for, request, g
 
 @app.route('/')
 def index():
     return "Hello World!"
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
-@app.route('/new_food_resource', methods=['GET', 'POST'])
-def register():
+@app.route('/new_food_resource', methods = ['GET', 'POST'])
+def add_resource():
     form = RequestNewFoodResourceForm(request.form)
     if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        #             form.password.data)
-        # db_session.add(user)
-        # flash('Thanks for registering')
-        # return redirect(url_for('login'))
         return "Hello World!"
     return render_template('add_resource.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return "Hello "+form.username.data
+    return render_template('login.html', form=form)
