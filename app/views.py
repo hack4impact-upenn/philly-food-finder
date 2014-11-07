82e8b5a7e1dc6d506d9cd7cc765dab30f7f8e420
natashanarang <nnarang777@gmail.com>
2014-11-07 18:14:33 -0500

    Added maps to html page

--- app/views.py
+++ app/views.py
@@ -1,22 +1,16 @@
 from app import app, db
 from models import Address
-<<<<<<< HEAD
-from flask import render_template
-=======
 from forms import RequestNewFoodResourceForm
 from flask import render_template, flash, redirect, session, url_for, request, g
->>>>>>> 4ec4491d79ed9c83c4e5014489c1966a55054fd6
 
 @app.route('/')
 def index():
     return "Hello World!"
 
-<<<<<<< HEAD
 @app.route('/map')
 def map():
-    return render_template('staticmap.html')
+    return render_template('newmaps.html')
 
-=======
 @app.route('/new_food_resource', methods=['GET', 'POST'])
 def register():
     form = RequestNewFoodResourceForm(request.form)
@@ -28,4 +22,3 @@ def register():
         # return redirect(url_for('login'))
         return "Hello World!"
     return render_template('add_resource.html', form=form)
->>>>>>> 4ec4491d79ed9c83c4e5014489c1966a55054fd6
