c7cdab7c1f5c6f7eeef2aaac9bd3a589f3766151
Ben Sandler <ben1sandler@gmail.com>
2014-11-14 18:39:42 -0500

    created login, logout, and began user invite/registration

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,9 @@ class ConfigClass(object):
     CSRF_ENABLED = True
 
     # Flask-Mail settings
-    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        'email@example.com')
-    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        'password')
-    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"MyApp" <noreply@example.com>')
+    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        'phillyhungercoalition@gmail.com')
+    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        '')
+    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"Foodle" <phillyhungercoalition@gmail.com>')
     MAIL_SERVER =             os.getenv('MAIL_SERVER',          'smtp.gmail.com')
     MAIL_PORT =           int(os.getenv('MAIL_PORT',            '465'))
     MAIL_USE_SSL =        int(os.getenv('MAIL_USE_SSL',         True))
@@ -27,9 +27,21 @@ class ConfigClass(object):
     USER_ENABLE_CHANGE_USERNAME    = False
     USER_ENABLE_FORGOT_PASSWORD    = True
     USER_ENABLE_CHANGE_PASSWORD    = True
-    USER_APP_NAME        = 'AppName'                # Used by email templates
+    USER_APP_NAME        = 'Foodle'                # Used by email templates
     USER_PASSWORD_HASH		= 'sha512_crypt'
     USER_PASSWORD_HASH_MODE          = 'passlib'
+
+    USER_CHANGE_PASSWORD_URL      = '/admin/change-password'
+    USER_CHANGE_USERNAME_URL      = '/admin/change-username'
+    USER_CONFIRM_EMAIL_URL        = '/admin/confirm-email/<token>'
+    USER_EMAIL_ACTION_URL         = '/admin/email/<id>/<action>'     
+    USER_FORGOT_PASSWORD_URL      = '/admin/forgot-password'
+    USER_LOGIN_URL                = '/admin/login'
+    USER_LOGOUT_URL               = '/admin/logout'
+    USER_MANAGE_EMAILS_URL        = '/admin/manage-emails'
+    USER_REGISTER_URL             = '/admin/invite'
+    USER_RESEND_CONFIRM_EMAIL_URL = '/admin/resend-confirm-email'    
+    USER_RESET_PASSWORD_URL       = '/admin/reset-password/<token>'
 
 app = Flask(__name__)
 app.config.from_object(__name__+'.ConfigClass')
@@ -39,8 +51,10 @@ db = SQLAlchemy(app) 	# Initialize Flask-SQLAlchemy
 mail = Mail(app)		# Initialize Flask-Mail
 
 from app import views, models, forms
+from forms import InviteForm
 from app.models import User
+from app.views import invite
 from flask_user import SQLAlchemyAdapter, UserManager
 
 db_adapter = SQLAlchemyAdapter(db, User)        # Register the User model
-user_manager = UserManager(db_adapter, app)     # Initialize Flask-User
+user_manager = UserManager(db_adapter, app, register_form = InviteForm, register_view_function = invite)     # Initialize Flask-User
