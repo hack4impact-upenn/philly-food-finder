4bea8b7181deda4fd237290b55c863fe5491d84d
veronicawharton <veronica.wharton@yahoo.com>
2014-11-14 15:14:24 -0500

    Added global variables.

--- app/views.py
+++ app/views.py
@@ -3,13 +3,14 @@ from models import Address, FoodResource, TimeSlot, User
 from forms import RequestNewFoodResourceForm
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from variables import resources_info
 
 @app.route('/')
 def index():
     return "Hello World!"
 
 @app.route('/new_food_resource', methods=['GET', 'POST'])
-def register():
+def new_food_resource():
     form = RequestNewFoodResourceForm(request.form)
     days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
         "Friday", "Saturday"]
@@ -27,13 +28,6 @@ def register():
 #@login_required
 def admin():
     resources = FoodResource.query.all()
-    resources_info = [
-        ["farmers-markets", "Farmers' Markets"], 
-        ["food-cupboards", "Food Cupboards"], 
-        ["meals-on-wheels", "Meals On Wheels"], 
-        ["share-host-sites", "SHARE Host Sites"], 
-        ["soup-kitchens", "Soup Kitchens"],
-        ["wic-offices", "WIC Offices"]]
     return render_template('admin.html', resources=resources, 
         resources_info=resources_info)
 
