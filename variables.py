7928e47502ec83ae7b2c2ad369f4ab19accd9b7e
veronicawharton <veronica.wharton@yahoo.com>
2014-11-14 15:14:24 -0500

    Added global variables.

---
+++ variables.py
@@ -0,0 +1,7 @@
+resources_info = [
+        {'id' : "farmers-markets", 'name' : "Farmers' Markets"}, 
+        {'id' : "food-cupboards", 'name' : "Food Cupboards"},
+        {'id' : "meals-on-wheels", 'name' : "Meals On Wheels"},
+        {'id' : "share-host-sites", 'name' : "SHARE Host Sites"},
+        {'id' : "soup-kitchens", 'name' : "Soup Kitchens"},
+        {'id' : "wic-offices", 'name' : "WIC Offices"}]
