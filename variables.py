38eb87e6c74dc0aa578995e36fcc72813167c430
veronicawharton <veronica.wharton@yahoo.com>
2014-11-14 15:26:24 -0500

    More global variables.

--- variables.py
+++ variables.py
@@ -1,7 +1,18 @@
-resources_info = [
+resources_info_plural = [
         {'id' : "farmers-markets", 'name' : "Farmers' Markets"}, 
         {'id' : "food-cupboards", 'name' : "Food Cupboards"},
         {'id' : "meals-on-wheels", 'name' : "Meals On Wheels"},
         {'id' : "share-host-sites", 'name' : "SHARE Host Sites"},
         {'id' : "soup-kitchens", 'name' : "Soup Kitchens"},
         {'id' : "wic-offices", 'name' : "WIC Offices"}]
+
+resources_info_singular = [
+        {'id' : "farmers-market", 'name' : "Farmers' Market"}, 
+        {'id' : "food-cupboard", 'name' : "Food Cupboard"},
+        {'id' : "meals-on-wheel", 'name' : "Meals On Wheel"},
+        {'id' : "share-host-site", 'name' : "SHARE Host Site"},
+        {'id' : "soup-kitchen", 'name' : "Soup Kitchen"},
+        {'id' : "wic-office", 'name' : "WIC Office"}]
+
+days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+        "Friday", "Saturday"]
