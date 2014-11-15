abe28407fc0228a5d32dab7861a5d65af947865d
Ben Sandler <ben1sandler@gmail.com>
2014-11-15 17:54:31 -0500

    full admin invite system is functional.

--- app/__init__.py
+++ app/__init__.py
@@ -4,24 +4,28 @@ import os
 from config import basedir
 from flask_mail import Mail
 
-# Use a Class-based config
+# Use a Class-based config to config flask and extensions
 class ConfigClass(object):
     # Flask settings
     SECRET_KEY =              os.getenv('SECRET_KEY',       'development key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     'sqlite:///basic_app.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     
+        'sqlite:///basic_app.sqlite')
     CSRF_ENABLED = True
 
     # Flask-Mail settings
-    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        'phillyhungercoalition@gmail.com')
-    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        'Q8qrHeTH')
-    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"Foodle" <phillyhungercoalition@gmail.com>')
-    MAIL_SERVER =             os.getenv('MAIL_SERVER',          'smtp.gmail.com')
+    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        
+        'phillyhungercoalition@gmail.com')
+    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        
+        '')
+    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  
+        '"Foodle" <phillyhungercoalition@gmail.com>')
+    MAIL_SERVER =             os.getenv('MAIL_SERVER',          
+        'smtp.gmail.com')
     MAIL_PORT =           int(os.getenv('MAIL_PORT',            '465'))
     MAIL_USE_SSL =        int(os.getenv('MAIL_USE_SSL',         True))
 
     # Flask-User settings
     USER_ENABLE_EMAIL              = True
-    USER_ENABLE_LOGIN_WITHOUT_CONFIRM           = True
     USER_REQUIRE_INVITATION        = True
     USER_ENABLE_USERNAME           = False
     USER_ENABLE_CHANGE_USERNAME    = False
@@ -30,6 +34,11 @@ class ConfigClass(object):
     USER_APP_NAME        = 'Foodle'                # Used by email templates
     USER_PASSWORD_HASH		= 'sha512_crypt'
     USER_PASSWORD_HASH_MODE          = 'passlib'
+    USER_CONFIRM_EMAIL_EXPIRATION    = 9223372036854775807
+    USER_RESET_PASSWORD_EXPIRATION   = 9223372036854775807
+
+    USER_AUTO_LOGIN_AFTER_REGISTER = False
+    USER_AUTO_LOGIN_AFTER_CONFIRM = True
 
     USER_CHANGE_PASSWORD_URL      = '/admin/change-password'
     USER_CHANGE_USERNAME_URL      = '/admin/change-username'
@@ -42,6 +51,9 @@ class ConfigClass(object):
     USER_REGISTER_URL             = '/admin/invite'
     USER_RESEND_CONFIRM_EMAIL_URL = '/admin/resend-confirm-email'    
     USER_RESET_PASSWORD_URL       = '/admin/reset-password/<token>'
+
+    USER_AFTER_REGISTER_ENDPOINT = 'invite_sent'
+    USER_AFTER_CONFIRM_ENDPOINT = 'user.change_password'
 
 app = Flask(__name__)
 app.config.from_object(__name__+'.ConfigClass')
@@ -57,4 +69,5 @@ from app.views import invite
 from flask_user import SQLAlchemyAdapter, UserManager
 
 db_adapter = SQLAlchemyAdapter(db, User)        # Register the User model
-user_manager = UserManager(db_adapter, app, register_form = InviteForm, register_view_function = invite)     # Initialize Flask-User
+user_manager = UserManager(db_adapter, app, register_form = InviteForm, 
+    register_view_function = invite)     # Initialize Flask-User
