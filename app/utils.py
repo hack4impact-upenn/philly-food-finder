f96bae415eb1b28179552e538fbcc5daf9b8acb8
Ben Sandler <ben1sandler@gmail.com>
2014-11-15 18:46:51 -0500

    fixed style, added comments

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,6 @@ import os, random, string
 
 # Function to generate a random password of given length 
 def generate_password(length):
-	chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+	chars = string.ascii_letters + string.digits + '!@#$%&'
 	random.seed = (os.urandom(1024))
 	return ''.join(random.choice(chars) for i in range(length-2)) + '1A'
