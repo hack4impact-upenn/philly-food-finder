d39d275300f3dbb5b19632a27e909b388e0177f2
veronicawharton <veronica.wharton@yahoo.com>
2014-11-16 12:02:19 -0500

    Timeslots are now stored correctly with FoodResources.

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,20 @@
 import os, random, string
+from datetime import time
 
 # Function to generate a random password of given length 
 def generate_password(length):
 	chars = string.ascii_letters + string.digits + '!@#$%&'
 	random.seed = (os.urandom(1024))
 	return ''.join(random.choice(chars) for i in range(length-2)) + '1A'
+
+# Takes strings of the form '8-00-am' or '1-15-pm' and returns an equivalent 
+# time object
+def get_time(time_string):
+	first_dash = time_string.index("-")
+	second_dash = time_string.index("-", first_dash+1)
+	time_hour = int(time_string[:first_dash])
+	time_minute = int(time_string[first_dash+1:second_dash])
+	am_or_pm = time_string[second_dash+1:]
+	if (am_or_pm == "pm"):
+		time_hour += 12
+	return time(time_hour, time_minute)
