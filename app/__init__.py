45ff87d3b49ccba041bf18ce9206c94d4ef0dbba
Ben Sandler <ben1sandler@gmail.com>
2014-11-27 15:39:34 -0500

    changes made for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Use a Class-based config to config flask and extensions
 class ConfigClass(object):
     # Flask settings
-    SECRET_KEY =              os.getenv('SECRET_KEY',       '\x86\xc8\xa9Z\tN\xb2\n\xce\x83\x0f\xef\x80\xd56\x948a\x92\xe5\xc5\xb1\xc5j')
+    SECRET_KEY =              os.getenv('SECRET_KEY',       '')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     
         'sqlite:///' + os.path.join(basedir, 'app.db'))
     CSRF_ENABLED = True
@@ -17,7 +17,7 @@ class ConfigClass(object):
     MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        
         'phillyhungercoalition@gmail.com')
     MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        
-        'Q8qrHeTH')
+        '')
     DEFAULT_MAIL_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  
         '"Foodle" <phillyhungercoalition@gmail.com>')
     MAIL_SERVER =             os.getenv('MAIL_SERVER',          
